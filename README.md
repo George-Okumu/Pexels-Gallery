4f406d8b5c2e9ac42878943ff15e0b12ba5fe892
George Okumu <gokumu12@gmail.com>
2021-06-04 07:24:22 +0300

    images displayed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Pexel-Gallery
 Website to search for favorite photo,
 
-## Prerequisites
-i) Made Using Pexel APi
+# Prerequisites
+### i) Made Using Pexel APi, Head over to [Pexels](https://www.pexels.com/join-consumer/) to create a new <br/> account in pexels
+### ii) Chakra Ui
+### iii) used Curated Endpoint to get trending images
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
+import Head from "next/head"
+import React, { useState } from "react";
+import { Box, Container, Text } from '@chakra-ui/react'
+import {getCuratedPhotos} from '../lib/api'
 
-export default function Home() {
+export default function Home({ data }) {
+  // console.log(data);
+  const [photos, setPhotos] = useState(data);
   return (
     <div>
+      <Head>
+        <title>Pexel Gallery</title>
+      </Head>
+      <Box overflow="hidden" bg="blue" minH="100vh">
+        <Container>
+          <Text color="white" textAlign="center" fontWeight="semibold" mb="1.5" pt="10" fontSize={["4xl", "4sm", "4sm", "5sm"]}>
+            Geo Pexel Gallery
+          </Text>
+          {
+  photos.map((pic) => (
+    <img src={pic.src.original} width="300" height="300" />
+  ))
+}
+        </Container>
+      </Box>
   
     </div>
   )
+}
+
+export async function getServerSideProps(){
+  const data = await getCuratedPhotos();
+  return {
+    props: {
+      data,
+    },
+  };
 }
